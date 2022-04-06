31d04238286fd6f84d8a1bd088c326e321168fd9
pasyagitka <lizavetazinovich@gmail.com>
2022-04-06 21:33:18 +0300

    app: add routers, 404 handling, global error handler

--- app.js
+++ app.js
@@ -5,22 +5,12 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const { ValidationError} = require('express-validator');
+const { ValidationError } = require('express-validation');
+
 require('dotenv').config();
 
 
 const app = express();
-
-const start = async() => {
-    try{
-        await mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-    } catch(e) {
-        console.log(e);
-    }
-}
 
 
 app.use(passport.initialize());
@@ -39,25 +29,46 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms', 
 }))
 
 const authRouter = require('./src/routes/auth');
-const ingredientsRouter = require('./src/routes/ingredients');
 const mealsRouter = require('./src/routes/meals');
 const categoriesRouter = require('./src/routes/categories');
+const recipesRouter = require('./src/routes/recipes');
+const ingredientsRouter = require('./src/routes/ingredients');
+const recipeIngredientsRouter = require('./src/routes/recipeIngredients');
+const cookbooksRouter = require('./src/routes/cookbooks');
 const textFromFileRouter = require('./src/routes/textFromFile');
 
 app.use('/auth', authRouter);
 app.use('/meals', mealsRouter);
-app.use('/ingredients', ingredientsRouter);
 app.use('/categories', categoriesRouter);
+app.use('/recipes', recipesRouter);
+app.use('/ingredients', ingredientsRouter);
+app.use('/recipeIngredients', recipeIngredientsRouter);
+app.use('/cookbooks', cookbooksRouter);
 app.use('/textFromFile', textFromFileRouter);
 
-
+app.use((req, res, next) => {
+    res.status(404).send("404: Not found");
+});
+  
 app.use((error, req, res, next) => { 
-    console.log(error);
+    if (error instanceof ValidationError) {
+        return res.status(error.statusCode).json(error.details.body[0].message);
+    }
+    if (!error.statusCode) error.statusCode = 500;
     res.status(error.statusCode).send(error.error);
 });
 
 
-
-start();
+const connectMongo = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    } catch(e) {
+        console.log(e);
+    }
+}
+connectMongo();
 
 module.exports = app;
